4c0058d361b0836b9bd0b566c6271332e2a872ac
jribon51 <151412716+jribon51@users.noreply.github.com>
2024-06-12 02:54:53 +0000

    [se termina el modelo completo] se agrega la tabla orders,reviews, providers se crean los primeros registros de información para la tabla users y providers se crear el primer endpont con metodo get para traer todos los providers, serealizazo un prueba y funciona correcatmente el endpoint /providers

--- src/api/models.py
+++ src/api/models.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__='user'
     id = db.Column(db.Integer, primary_key=True)
-    username=db.Column(db.String,unique=True,nullable=False)
+    username=db.Column(db.String(50),unique=True,nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     role=db.Column(db.String(50),nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
@@ -13,7 +13,7 @@ class User(db.Model):
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return f'User {self.username} {self.email} {self.role} {self.create_at} {self.is_active}'
+        return f'User {self.id}  {self.username} {self.email} {self.role} {self.create_at} {self.is_active}'
 
     def serialize(self):
         return {
@@ -32,14 +32,14 @@ class UserProfiles(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id=db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
     user=db.relationship(User);
-    first_name=db.Column(db.String,nullable=False)
-    last_name=db.Column(db.String,nullable=False)
+    first_name=db.Column(db.String(50),nullable=False)
+    last_name=db.Column(db.String(50),nullable=False)
     bio=db.Column(db.String(50),nullable=False)
     location=db.Column(db.String(50),nullable=False)
     profile_picture=db.Column(db.String(150),nullable=False)
 
     def __repr__(self):
-        return f'User {self.first_name} {self.last_name} {self.bio} {self.location} {self.profile_picture}'
+        return f'User {self.id}  {self.user_id}  {self.first_name} {self.last_name} {self.bio} {self.location} {self.profile_picture}'
 
     def serialize(self):
         return {
@@ -52,3 +52,82 @@ class UserProfiles(db.Model):
             "profile_picture":self.profile_picture             
             # do not serialize the password, its a security breach
         }
+        
+    
+class Providers(db.Model):
+    __tablename__='providers'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id=db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
+    user=db.relationship(User);
+    profession=db.Column(db.String(50),nullable=False)
+    description=db.Column(db.String(150),nullable=False)
+    price=db.Column(db.Integer,nullable=False)
+    category=db.Column(db.String(50),nullable=False)
+    create_at=db.Column(db.Date, unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'User {self.user_id} {self.profession} {self.description} {self.price} {self.category} {self.create_at}'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id":self.user_id,
+            "profession": self.profession,
+            "description":self.description,
+            "price":self.price,
+            "category":self.category,
+            "create_at":self.create_at             
+            # do not serialize the password, its a security breach
+        }  
+        
+        
+class Orders(db.Model):
+    __tablename__='orders'
+    id = db.Column(db.Integer, primary_key=True)
+    providers_id=db.Column(db.Integer,db.ForeignKey('providers.id'),nullable=False)
+    providers=db.relationship(Providers);
+    client_id=db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
+    client=db.relationship(User)
+    status=db.Column(db.String(50),nullable=False)
+    order_date=db.Column(db.Date, unique=False, nullable=False)
+    completion_date=db.Column(db.Date, unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'User {self.id} {self.providers_id} {self.client_id} {self.status} {self.order_date} {self.completion_date}'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "providers_id":self.providers_id,
+            "client_id": self.client_id,
+            "description":self.description,
+            "status":self.status,
+            "order_date":self.order_date,
+            "completion_date":self.completion_date             
+            # do not serialize the password, its a security breach
+        } 
+
+class Reviews(db.Model):
+    __tablename__='reviews'
+    id = db.Column(db.Integer, primary_key=True)
+    order_id=db.Column(db.Integer,db.ForeignKey('orders.id'),nullable=False)
+    order=db.relationship(Orders);
+    rating=db.Column(db.String(50),nullable=False)
+    comment=db.Column(db.String(50),nullable=False)
+    created_at=db.Column(db.Date, unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'User {self.id}  {self.order_id}  {self.rating} {self.comment} {self.created_at}'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "order_id":self.order_id,
+            "rating": self.rating,
+            "last_name":self.last_name,
+            "comment":self.comment,
+            "created_at":self.created_at          
+            # do not serialize the password, its a security breach
+        }
+        
+
