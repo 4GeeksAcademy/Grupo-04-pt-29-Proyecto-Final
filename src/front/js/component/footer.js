be3683c5d33f245e6f4db25f51adf5ff1c5a2777
jribon51 <151412716+jribon51@users.noreply.github.com>
2024-06-15 02:19:24 +0000

    [se corrige el footer] queda responsive y con las fuentes que faltaban

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,14 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
+import '../../styles/footer.css';
 
 export const Footer = () => (
 	<footer className="footer-app footer text-center pt-5">
 		<div className="container pt-4">
-			<div className="contenedor d-flex justify-content-between row">
-				<div className="columna-1 col-5">
+			<div className="row">
+				<div className="columna-1 col-12 col-md-5 mb-4 mb-md-0">
 					<h5 className="text-principal">cyber</h5>
 					<p>we are a residential interior design firm located in Portland. Our boutique-studio offers more than</p>
 				</div>
-				<div className="columna-2 col-3">
+				<div className="columna-2 col-12 col-md-3 mb-4 mb-md-0">
 					<h5 className="text-titulo">Services</h5>
 					<ul className="list-unstyled">
 						<li>Bonus program</li>
@@ -19,7 +20,7 @@ export const Footer = () => (
 						<li>Payment</li>
 					</ul>
 				</div>
-				<div className="columna-3 col-4 text-titulo">
+				<div className="columna-3 col-12 col-md-4">
 					<h5 className="text-titulo">Assistance to the buyer</h5>
 					<ul className="list-unstyled">
 						<li>Find an order</li>
@@ -31,13 +32,14 @@ export const Footer = () => (
 					</ul>
 				</div>
 			</div>
-			<div className="iconos-footer d-flex justify-content-between ">
-				<i className="fa-brands fa-twitter"></i>
-				<i class="fa-brands fa-facebook-f"></i>
-				<i class="fa-brands fa-tiktok"></i>
-				<i class="fa-brands fa-instagram"></i>
+			<div className="footer-class ">
+				<div className="iconos-footer  d-flex justify-content-between"> 
+					<i className="fa-brands fa-twitter"></i>
+					<i class="fa-brands fa-facebook-f"></i>
+					<i class="fa-brands fa-tiktok"></i>
+					<i class="fa-brands fa-instagram"></i>
+				</div>
 			</div>
 		</div>
-
 	</footer>
 );
