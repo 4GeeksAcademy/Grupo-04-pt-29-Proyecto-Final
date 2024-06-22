1c2aea854e60b992cea82740bb6c8b3a4aaacac0
jribon51 <151412716+jribon51@users.noreply.github.com>
2024-06-22 03:29:09 +0000

    se realizan los cards con el fetch, se debe revisar como se ve en otros resoluciones

--- src/front/js/component/serviciosPopulares.js
+++ src/front/js/component/serviciosPopulares.js
@@ -1,26 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../../styles/serviciosPopulares.css"
 
 
-
-
-
-
-
-
 const Serviciospopulares = () => {
+    const [providers, setProviders] = useState([])
+    console.log("se imprime providers", providers)
+
+
+
+    useEffect(() => {
+
+        fetch(`${process.env.BACKEND_URL}providers`)
+            .then(response => response.json())
+            .then(data => {
+                console.log("data", data)
+                setProviders(data.data)
+            })
+            .catch(error => {
+                console.error("Error al traer lista de  providers:", error);
+            })
+    }, [])
+
     return (
-        <div classNameName="container">
-            <div className="card" style={{ "width": "15rem" }}>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdx38W42k6ujIXAghZNamkWwcMPEXWZbb8CA&s" className="card-img-top" alt="..." />
-                <div className="color-card card-body text-center">
-                    <h5 className="h5-descripcion card-title text-center font-weight-bold">Servicio de Limpieza</h5>
-                    <h2 className="valor-card card-title text-center ">$15</h2>
-                    <a href="#" className="btn-minfo btn">Más Información</a>
+        <div className="container">
+            <div className="content-header text-center">
+                <h3>Servicios Populares</h3>
+            </div>
+            <div className="mb-4 linea" style={{ "width": "50px", "height": "5px", "background": "#508DF7" }}></div>
+            <div className="div-center text-center">
+                <div className="row row-content m-auto">
+                    {providers.map((value, index) => {
+                        return (
+                            <div className="col">
+                                <div key={index} className="card mb-5" style={{ "width": "15rem" }}>
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdx38W42k6ujIXAghZNamkWwcMPEXWZbb8CA&s" className="card-img-top" alt="..." />
+                                    <div className="color-card card-body text-center">
+                                        <h5 className="h5-descripcion card-title text-center font-weight-bold">{value.profession}</h5>
+                                        <h2 className="valor-card card-title text-center ">${value.price}</h2>
+                                        <a href="#" className="btn-minfo btn">Más Información</a>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
-        </div>
 
+        </div>
     )
 }
 
