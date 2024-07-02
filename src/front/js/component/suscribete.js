d3b3c26d9d70ab0a72768bf799869297f0929c7f
jribon51 <151412716+jribon51@users.noreply.github.com>
2024-07-02 01:15:42 +0000

    se envian correciones de footer y suscribete

--- src/front/js/component/suscribete.js
+++ src/front/js/component/suscribete.js
@@ -10,13 +10,13 @@ const Suscribete = () => {
 
             <section className="suscribete">
                 <div className="fondo-gradiente">
-                    <div className="container py-5 d-flex justify-content-center align-items-center" style={{ "height": "400px" }}>
-                        <div>
-                            <h1 className="h1-suscribete">Suscríbete</h1>
+                    {/* <div className="container py-5 d-flex flex-column align-items-center" style={{ "height": "400px" }}> */}
+                        <div className="container d-flex flex-column align-items-center pt-5" style={{ "height": "400px" }}>
+                            <h1 className="h1-suscribete mt-5">Suscríbete</h1>
                             <div className="row title-parrafo">recibe las nuevas actualizaciones, ofertas y <br /> tips para mejorar tu experiencia</div>
                             <button className="button-style mt-4">Tu Email</button>
                         </div>
-                    </div>
+                    {/* </div> */}
                 </div>
             </section >
 
