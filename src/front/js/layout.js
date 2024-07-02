e232679e91cc271d6549afd55a34760780a60a39
Luis Giuliani <154923735+jrgiuliani@users.noreply.github.com>
2024-07-02 02:08:54 +0000

    actualizacion rama

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -41,8 +41,9 @@ const Layout = () => {
 
             <Route element={<PrivateRoutes/>}>
               {/* Aqui van todas las rutas que quiero Proteger *(las Rutas Privadas) */}
+              <Route element={<ProductsPage />} path="/productspage" />
             </Route>
-            <Route element={<ProductsPage />} path="/productspage" />
+            
             <Route element={<Login />} path="/login" />
             <Route element={<h1>Not found!</h1>} />
           </Routes>
