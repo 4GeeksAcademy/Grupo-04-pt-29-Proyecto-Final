6553e534f144cbe1c2d6752322c8014d4267dd57
Luis Giuliani <154923735+jrgiuliani@users.noreply.github.com>
2024-07-05 02:04:27 +0000

    funcionalidad de en prouct page filtros

--- src/front/js/component/listaServicios.js
+++ src/front/js/component/listaServicios.js
@@ -3,50 +3,80 @@ import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 import "../../styles/listaServicios.css";
 
-export const ListaServicios = () => {
-    const { store, actions } = useContext(Context);
-    useEffect(() => {
-        actions.getProviders()
-    }, [])
+export const ListaServicios = ({ data, filters }) => {
+    const filteredData = data.filter(item => {
+        const matchesCategory =
+          !filters.category ||
+          item.services.some(service =>
+            service.category.toLowerCase().includes(filters.category.toLowerCase())
+          );
+    
+        const matchesPrice =
+        !filters.price ||
+        item.services.some(service => service.price == filters.price)
+        
 
+        const matchesLocation =
+          !filters.location ||
+          item.location.toLowerCase().includes(filters.location.toLowerCase());
 
-    console.log(store.listProviders);
+        const matchesSelectedCategories =
+            filters.selectedCategories.length === 0 ||
+            item.services.some(service =>
+                filters.selectedCategories.includes(service.category)
+            );
 
-    const e = store.listProviders && store.listProviders.flatMap((provider, index) => {
+            
+        const matchesSelectedPrices =
+            filters.selectedPrices.length === 0 ||
+            item.services.some(service =>
+                filters.selectedPrices == service.price
+            );
 
-        return provider.services.map((service, serviceIndex) => {
-            return <div key={`${index} + ${serviceIndex}`} className="card mb-3 tarjeta-listaServ" style={{ maxWidth: "810px" }}>
-                <div className="row g-0">
-                    <div className="col-md-4">
-                        <img className="img-fluid rounded-start foto-listaServ" src={provider.url_image} alt="..." />
-                        <p className="card-text categoria-listaServ"><small className="text-body-secondary">{service.category}</small></p>
-                    </div>
-                    <div className="col-md-8">
-                        <div className="card-body">
-                            <div className="d-flex justify-content-between">
-                                <h5 className="card-title titulo-listaServ">{service.title}</h5>
-                                <p className="precio-listaServ">
-                                    <i className="fa-solid fa-dollar-sign icono-listaServ">
-                                    </i>{service.price}</p>
-                            </div>
-                            <h6 className="card-title nombre-listaServ">{provider.name} {provider.last_name}</h6>
-                            <p className="card-text textoTarjeta-listaServ">{service.description}</p>
-                            <div className="d-flex justify-content-between mt-5">
-                                <p className="card-text textoIconos-listaServ"><small className="text-body-secondary"> <i className="fa-solid fa-location-dot icono-listaServ"></i>  {provider.location}</small></p>
-                                <p className="card-text mx-2 textoIconos-listaServ"><small className="text-body-secondary">
-                                    Valoración 
-                                    {[...Array(provider.valoration)].map((e, i) => {
-                                        return <i key={i} className="fa-regular fa-star icono-listaServ" />
-                                    })}
-                                </small></p>
-                                <p className="card-text ml-auto textoIconos-listaServ favorito-listaServ"><small className="text-body-secondary"> Guardar  <i className="fa-regular fa-heart corazon-listaServ"></i></small></p>
-                            </div>
+        const matchesSelectedLocations =
+            filters.selectedLocations.length === 0 ||
+            filters.selectedLocations.includes(item.location)
+            
+    
+        return matchesCategory && matchesPrice && matchesLocation && matchesSelectedCategories && matchesSelectedPrices && matchesSelectedLocations;
+      });
+
+      
+    const e = filteredData.map((item) => (
+         <div key={` ${item.id}`} className={`card mb-3 tarjeta-listaServ ${item.id}`} style={{ maxWidth: "1200px" }}>
+            {item.services.map((service) => ( 
+            <div className="row g-0" key={`${service.id}${service.provider_id}`}>
+                <div className="col-md-4">
+                    <img className="img-fluid rounded-start foto-listaServ" src={service.url_image} alt="..." />
+                    <p className="card-text categoria-listaServ"><small className="text-body-secondary">{item.category}</small></p>
+                </div>
+                <div className="col-md-8">
+                    <div className="card-body">
+                        <div className="d-flex justify-content-between">
+                            <h5 className="card-title titulo-listaServ">{item.title}</h5>
+                            <p className="precio-listaServ">
+                                <i className="fa-solid fa-dollar-sign icono-listaServ">{service.price}</i>
+                            </p>
+                        </div>
+                        <h6 className="card-title nombre-listaServ">{item.name} {item.last_name}</h6>
+                        <p className="card-text textoTarjeta-listaServ">{service.description}</p>
+                        <div className="d-flex justify-content-between mt-5">
+                            <p className="card-text textoIconos-listaServ"><small className="text-body-secondary"> <i className="fa-solid fa-location-dot icono-listaServ"></i>  {item.location}</small></p>
+                            <p className="card-text mx-2 textoIconos-listaServ"><small className="text-body-secondary">
+                                Valoración 
+                                    {[...Array(service.valoration)].map((e, i) => {
+                                    return <i key={i} className="fa-regular fa-star icono-listaServ" />
+                                })} 
+                                </small></p> 
+                            <p className="card-text ml-auto textoIconos-listaServ favorito-listaServ"><small className="text-body-secondary"> Guardar  <i className="fa-regular fa-heart corazon-listaServ"></i></small></p>
                         </div>
                     </div>
                 </div>
             </div>
-        })
-    })
+            ))}
+        </div>
+    ))
+    
     return e
 }
 
--- src/front/js/pages/productsPage.js
+++ src/front/js/pages/productsPage.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Context } from "../store/appContext";
 import { ListaServicios } from "../component/listaServicios";
 import { BarraCategorias } from "../component/barraCategorias";
@@ -6,16 +6,35 @@ import { BarraCategorias } from "../component/barraCategorias";
 
 
 export const ProductsPage = () => {
+	const { store, actions } = useContext(Context);
+
+    useEffect(() => {
+        actions.getProviders()
+    }, [])
+
+	const [filters, setFilters] = useState({
+		category: '',
+		price: '',
+		location: '',
+		selectedCategories: [],
+		selectedPrices: [],
+		selectedLocations: [],
+	});
+	
+	const handleFilterChange = (newFilters) => {
+		setFilters(newFilters);
+	};
+	const data = store.listProviders
 
 	return (
 		<div className="container"> 
 			<div className="pt-3 pb-3"> Inicio - Servicios Disponibles </div> 
 			<div className="d-flex">
 				<div className="m-3">
-					<BarraCategorias />
+					<BarraCategorias filters={filters} onFilterChange={handleFilterChange} />
 				</div>
 				<div className="m-3">
-					<ListaServicios />
+					<ListaServicios data={data} filters={filters} />
 				</div>
 			</div>
 		</div>
